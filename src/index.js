755a31d47afa15d7d05dc70ed9ac21c92bcd1fbe
luopd <luopeidong@medlinker.com>
2018-06-22 17:55:04 +0800

    'login-page'

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import 'promise-polyfill/src/polyfill';
-import './assets/css/common.css';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
+import './assets/css/common.css';
 
 render(<App />, document.getElementById('root'));
 
